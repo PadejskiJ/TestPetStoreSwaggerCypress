e531b4f7d2409910978be2ca5da04d67a5c9372a
PadejskiJ <jelena.padejski@gmail.com>
2022-11-23 09:11:37 +0100

    first test

---
+++ cypress/e2e/TestPetStore.spec.cy.ts
@@ -0,0 +1,165 @@
+describe("pet store swagger api", () => {
+  it("get inventory", () => {
+    cy.request({
+      method: "GET",
+      url: "https://petstore.swagger.io/v2/store/inventory",
+      headers: {
+        Authorisation: "special-key",
+      },
+    }).then((response) => {
+      expect(response.status).equal(200);
+    });
+  });
+  //how to add photo?
+  it("post - add pet photo", () => {
+    cy.request({
+      method: "POST",
+      url: "https://petstore.swagger.io/v2/pet/uploadFile",
+      headers: {
+        Authorisation: "special-key",
+      },
+      body: {
+        petID: "111",
+        additionalMetadata: "dog",
+        file: "",
+      },
+    }).then((response) => {
+      expect(response.status).equal(200);
+    });
+  });
+
+  it("post - add new pet to the store", () => {
+    cy.request({
+      method: "POST",
+      url: "https://petstore.swagger.io/v2/pet",
+      headers: {
+        Authorisation: "special-key",
+      },
+      body: {
+        id: 111,
+        category: {
+          id: 101,
+          name: "dog",
+        },
+        name: "leksi",
+        photoUrls: ["string"],
+        tags: [
+          {
+            id: 1,
+            name: "dog",
+          },
+        ],
+        status: "available",
+      },
+    }).then((response) => {
+      expect(response.status).equal(200);
+    });
+  });
+
+  it("put - update an existing pet in the store", () => {
+    cy.request({
+      method: "PUT",
+      url: "https://petstore.swagger.io/v2/pet",
+      headers: {
+        Authorisation: "special-key",
+      },
+      body: {
+        id: 111,
+        category: {
+          id: 101,
+          name: "doggie",
+        },
+        name: "Leksi",
+        photoUrls: ["string"],
+        tags: [
+          {
+            id: 1,
+            name: "dog",
+          },
+        ],
+        status: "available",
+      },
+    }).then((response) => {
+      expect(response.status).equal(200);
+    });
+  });
+
+  it("get - finds pets by status", () => {
+    cy.request({
+      method: "GET",
+      url: "http://petstore.swagger.io/v2/pet/findByStatus?status=pending",
+      headers: {
+        Authorisation: "special-key",
+      },
+    }).then((response) => {
+      expect(response.status).equal(200);
+    });
+  });
+
+  it("get - finds pet by id", () => {
+    cy.request({
+      method: "GET",
+      url: "http://petstore.swagger.io/v2/pet/111",
+      headers: {
+        Authorisation: "special-key",
+      },
+    }).then((response) => {
+      expect(response.status).equal(200);
+    });
+  });
+  //insert variable petID
+  it("post - updates a pet in the store with form data", () => {
+    cy.request({
+      method: "POST",
+      url: "https://petstore.swagger.io/v2/pet/111",
+      headers: {
+        Authorisation: "special-key",
+      },
+      body: {
+        id: 111,
+        category: {
+          id: 101,
+          name: "dog",
+        },
+        name: "leksi",
+        photoUrls: ["string"],
+        tags: [
+          {
+            id: 1,
+            name: "dog",
+          },
+        ],
+        status: "sold",
+      },
+    }).then((response) => {
+      expect(response.status).equal(200);
+    });
+  });
+  it("delete - deletes a pet from the store", () => {
+    cy.request({
+      method: "DELETE",
+      url: "https://petstore.swagger.io/v2/pet/111",
+      headers: {
+        Authorisation: "special-key",
+      },
+      body: {
+        id: 111,
+        category: {
+          id: 101,
+          name: "dog",
+        },
+        name: "leksi",
+        photoUrls: ["string"],
+        tags: [
+          {
+            id: 1,
+            name: "dog",
+          },
+        ],
+        status: "sold",
+      },
+    }).then((response) => {
+      expect(response.status).equal(200);
+    });
+  });
+});
